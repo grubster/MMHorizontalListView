695310cfff6c1dafdf5a0c06f5eba55ca762544d
Pedro Souza <pedronicholas.souza@gmail.com>
2015-01-13 10:42:12 -0200

    moving files for Pods hierarchy folder

---
+++ Example/Pods/Target Support Files/Pods-MMHorizontalListView-MMHorizontalListView/Pods-MMHorizontalListView-MMHorizontalListView-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MMHorizontalListView_MMHorizontalListView : NSObject
+@end
+@implementation PodsDummy_Pods_MMHorizontalListView_MMHorizontalListView
+@end
---
+++ Example/Pods/Target Support Files/Pods-MMHorizontalListView/Pods-MMHorizontalListView-environment.h
@@ -0,0 +1,14 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// MMHorizontalListView
+#define COCOAPODS_POD_AVAILABLE_MMHorizontalListView
+#define COCOAPODS_VERSION_MAJOR_MMHorizontalListView 0
+#define COCOAPODS_VERSION_MINOR_MMHorizontalListView 1
+#define COCOAPODS_VERSION_PATCH_MMHorizontalListView 0
+
